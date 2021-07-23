0e142762a17cfa691a14da070dd193fb17aa0397
TimZoe <contact@trophie.de>
2021-07-23 14:54:45 +0200

    FEFrame

--- src/App.js
+++ src/App.js
@@ -3,21 +3,103 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="AppWrapper">
+
+
+        <div class="functionWrapper">
+          <h1>User Information</h1>
+          <div class="h2Wrap">
+            <h2>Eigenloggt ist</h2> <h2>UserName</h2>
+          </div>
+          <div class="h2Wrap">
+            <h2>Attribut 2 ist</h2> <h2>Attribut 2</h2>
+          </div>
+          
+        </div>
+
+
+
+
+
+
+        <div class="functionWrapper">
+          
+          <h1>Card Informationen</h1>
+
+          <div id="cardWrapperWrap">
+            <div id="cardImg">
+              <h2>CardImage</h2>
+            </div>
+          <div id="CardInfo">
+             
+             <div class="h2Wrap">
+             <h2>Card ID</h2> <h2>Card ID</h2>
+             </div>
+             <div class="h2Wrap">
+             <h2>Card Preis</h2> <h2>Card Preis</h2>
+             </div>
+          </div>
+         
+            
+          </div>
+        </div>
+
+
+
+
+
+
+
+        <div class="functionWrapper">
+          <h1>Card Wrapper</h1>
+          <div class="h2Wrap">
+            <h2>Show 3 Karten Wrapper</h2>
+          </div>
+          
+
+        </div>
+
+
+
+
+
+
+
+        <div class="functionWrapper">
+
+          <h1>Scores</h1>
+          <div class="h2Wrap">
+            <h2>Trophie Score ist</h2> <h2>Trophie Score</h2>
+          </div>
+          <div class="h2Wrap">
+            <h2>Wert der Karten ist</h2> <h2>Karten Wert</h2>
+          </div>
+          
+          </div>
+
+
+
+
+
+
+
+
+        <div class="functionWrapper">
+
+          <h1>Countdown</h1>
+          <div class="h2Wrap">
+            <h2>Letztes Mal geöffnet am</h2> <h2>LastOpenDate</h2>
+          </div>
+          <div class="h2Wrap">
+            <h2>Countdown</h2> <h2>Countdown</h2>
+          </div>
+          
+
+        </div>
+    
+      </div>
+
     </div>
   );
 }
